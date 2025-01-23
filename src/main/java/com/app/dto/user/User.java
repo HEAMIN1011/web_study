40793e8c20bff447281ac926c3404bdd2b4c0bca
HEAMIN1011 <mintwo20@nate.com>
2025-01-23 09:30:39 +0900

    로그인, 로그아웃, 마이페이지 추가

--- src/main/java/com/app/dto/user/User.java
+++ src/main/java/com/app/dto/user/User.java
@@ -4,10 +4,14 @@ import lombok.Data;
 
 @Data
 public class User {
-
+	
+//	public static final String USERTYPE_CUSTOMER = "CUS";
+//	public static final String USERTYPE_ADMIN = "ADM";
+	
 	String id;
 	String pw;
 	String name;
 	String userType;  // CUS(Customer)   ADM(Admin)
 	
 }
+
--- src/main/webapp/WEB-INF/views/customer/mypage.jsp
+++ src/main/webapp/WEB-INF/views/customer/mypage.jsp
@@ -7,9 +7,13 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>quiz main</h1>
+	<h1>사용자 마이페이지</h1>
 	
-	${data1}
-	${data2}
+	<p>	아이디 : ${user.id} </p>
+	<p> 이름 :  ${user.name} </p>
+	<p>
+		<a href="/main">메인화면으로</a>
+	</p>
+	
 </body>
 </html>
